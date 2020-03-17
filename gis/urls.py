a0f86f85a60e3374a09c29a7735914942d3653c0
Vsevolod Demidov <fondad@ukr.net>
2020-03-17 12:23:22 +0200

    For gis app urls was created. Views home and template home was created.

--- gis/urls.py
+++ gis/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
-from django.urls import include
+import gis.views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('gis.urls'))
+    url(r'^$', gis.views.home, name='home'),
 ]
